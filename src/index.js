607bfa598125c08981cb2cacf2ee01b3438f31ff
dyq666 <694526892@qq.com>
2019-03-12 21:20:58 +0800

    Add iconfont & import in index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-import './static/reset.css';
 import 'antd/dist/antd.css';
+import './static/reset.css';
+import './static/iconfont/iconfont.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
