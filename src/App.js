f3b084a4fb1666f60f3dca31bb01346ada8ec68d
dyq666 <694526892@qq.com>
2019-03-13 18:39:04 +0800

    Use router switch index page & detail page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,63 @@
-import React, { Component } from 'react';
+import { Card, Input, Button } from 'antd';
+import React, { Fragment, Component } from 'react';
+import { Link, Route, BrowserRouter } from 'react-router-dom';
 
 import TodoList from './TodoList';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <TodoList />
+      <BrowserRouter>
+        <Fragment>
+          <RouteSwitch />
+          <Route path='/' exact component={TodoList} />
+          <Route path='/detail/:id' exact component={Detail} />
+        </Fragment>
+      </BrowserRouter>
+    );
+  }
+}
+
+const Detail = (props) => {
+  return (
+    <Card style={{width: '300px', marginTop: '20px', marginLeft: '20px'}}>
+      <p>I am detail.</p>
+      <p>My id is {props.match.params.id}</p>
+    </Card>
+  );
+};
+
+class RouteSwitch extends Component {
+
+  state = {
+    detailId: ''
+  };
+
+  render() {
+    const { detailId } = this.state;
+
+    return (
+      <div style={{marginTop: '20px', marginLeft: '20px'}}>
+        <Link to='/'>
+          <Button style={{marginBottom: '10px', display: 'block'}}>
+            返回首页
+          </Button>
+        </Link>
+        <Input
+          style={{width: '300px'}}
+          value={detailId}
+          onChange={this.handleInputChange}
+          placeholder='输入 id'
+        />
+        <Link to={`/detail/${detailId}`}>
+          <Button style={{marginLeft: '10px'}}>去详情页</Button>
+        </Link>
       </div>
     );
   }
+
+  handleInputChange = (e) => this.setState({ detailId: e.target.value });
+
 }
 
 export default App;
